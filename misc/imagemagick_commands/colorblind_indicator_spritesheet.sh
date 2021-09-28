432d501d62194997a534f90d97aa7cbfc82ce0af
normaljosh <normaljosh@gmail.com>
2021-09-28 12:46:18 -0500

    make hexagon icons for base cards

---
+++ misc/imagemagick_commands/colorblind_indicator_spritesheet.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+folder_path="../card_shapes"
+magick montage $folder_path/{blue,green,red,yellow,purple}_shape.png -tile 5x1 -geometry +5+5 -background 'white' $folder_path/colorblind_indicator_spritesheet.png
---
+++ misc/imagemagick_commands/generate_hex_script.py
@@ -0,0 +1,27 @@
+# Small script to generate shell script text for hexagon extraction 
+# Note that the hex positions still need to be manually changed
+
+# Offsets to match 
+offset_list = [6,-2,0,-4,0]
+
+# Make lines for age 2-10
+color_list = ['BLUE', 'RED', 'GREEN', 'YELLOW', 'PURPLE']
+with open ("hex_extract_stub.txt", "w") as f:
+    for i in range(16,106):
+        
+        #Write age number in every chunk of 10
+        if (i-16) %10 == 0:
+            f.write("\n")
+            f.write(f"# Age {(i-16)//10 + 2}")
+            f.write("\n")
+            
+        # Get offset for read file
+        offset_index = (i-16)%10 //2
+        read_num = str(i+offset_list[offset_index]).zfill(3)
+        read_file = f'"Print_BaseCards_front-{read_num}.png"'
+        write_num = str(i-1).zfill(3)
+        color = color_list[(i-16) % 10 // 2]
+        
+        # Write to file
+        f.write("\t".join(["make_hexagon", read_file, '"$TOP_LEFT"',  f'"${color}_BORDER"', f'"{write_num}.png"']))
+        f.write("\n")
