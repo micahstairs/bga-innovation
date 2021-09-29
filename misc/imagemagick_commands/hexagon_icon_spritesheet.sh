67ff2b6128f630f8b1a0cced86148ac34a22849d
normaljosh <normaljosh@gmail.com>
2021-09-29 16:45:29 -0500

    resize, do one row at a time

--- misc/imagemagick_commands/hexagon_icon_spritesheet.sh
+++ misc/imagemagick_commands/hexagon_icon_spritesheet.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 folder_path="../hexagon_icons"
 
-magick montage $folder_path/{000..014}.png -tile 15x1 -geometry +5+5 -background 'none' $folder_path/base_hexagon_spritesheet_age_1.png
-
-magick montage $folder_path/{015..024}.png -tile 10x1 -geometry +5+5 -background 'none' $folder_path/base_hexagon_spritesheet_age_2.png
-magick montage $folder_path/base_hexagon_spritesheet_age_2.png ../card_shapes/colorblind_indicator_spritesheet.png -tile 2x1 -geometry +5+5 -background 'none' $folder_path/base_hexagon_spritesheet_age_2.png
-
-magick montage $folder_path/{025..034}.png -tile 10x1 -geometry +5+5 -background 'none' $folder_path/base_hexagon_spritesheet_age_3.png
-magick montage $folder_path/base_hexagon_spritesheet_age_3.png ../special_achievement_icons/special_achievement_icons.png -tile 2x1 -geometry +5+5 -background 'none' $folder_path/base_hexagon_spritesheet_age_3.png
-
-magick montage $folder_path/{035..044}.png -tile 10x1 -geometry +5+5 -background 'none' $folder_path/base_hexagon_spritesheet_age_4.png
-magick montage $folder_path/base_hexagon_spritesheet_age_4.png ../special_achievement_icons/rotated_special_achievement_icons.png -tile 2x1 -geometry +5+5 -background 'none' $folder_path/base_hexagon_spritesheet_age_4.png
-
-magick montage $folder_path/{045..054}.png -tile 10x1 -geometry +5+5 -background 'none' $folder_path/base_hexagon_spritesheet_age_5.png
-magick montage $folder_path/{055..064}.png -tile 10x1 -geometry +5+5 -background 'none' $folder_path/base_hexagon_spritesheet_age_6.png
-magick montage $folder_path/{065..074}.png -tile 10x1 -geometry +5+5 -background 'none' $folder_path/base_hexagon_spritesheet_age_7.png
-magick montage $folder_path/{075..084}.png -tile 10x1 -geometry +5+5 -background 'none' $folder_path/base_hexagon_spritesheet_age_8.png
-magick montage $folder_path/{085..094}.png -tile 10x1 -geometry +5+5 -background 'none' $folder_path/base_hexagon_spritesheet_age_9.png
-magick montage $folder_path/{095..104}.png -tile 10x1 -geometry +5+5 -background 'none' $folder_path/base_hexagon_spritesheet_age_10.png
+magick montage $folder_path/{000..014}.png -tile 15x1 -geometry 120x120+5+5 -background 'none' $folder_path/base_hexagon_spritesheet_age_1.png
+magick montage $folder_path/{015..024}.png ../card_shapes/{blue,green,red,yellow,purple}_shape.png -tile 15x1 -geometry 120x120+5+5 -background 'none' $folder_path/base_hexagon_spritesheet_age_2.png
+magick montage $folder_path/{025..034}.png ../special_achievement_icons//{empire,monument,universe,wonder,world}.png -tile 15x1 -geometry 120x120+5+5 -background 'none' $folder_path/base_hexagon_spritesheet_age_3.png
+magick montage $folder_path/{035..044}.png ../special_achievement_icons//{empire,monument,universe,wonder,world}_portrait.png -tile 15x1 -geometry 120x120+5+5 -background 'none' $folder_path/base_hexagon_spritesheet_age_4.png
+magick montage $folder_path/{045..054}.png -tile 10x1 -geometry 120x120+5+5 -background 'none' $folder_path/base_hexagon_spritesheet_age_5.png
+magick montage $folder_path/{055..064}.png -tile 10x1 -geometry 120x120+5+5 -background 'none' $folder_path/base_hexagon_spritesheet_age_6.png
+magick montage $folder_path/{065..074}.png -tile 10x1 -geometry 120x120+5+5 -background 'none' $folder_path/base_hexagon_spritesheet_age_7.png
+magick montage $folder_path/{075..084}.png -tile 10x1 -geometry 120x120+5+5 -background 'none' $folder_path/base_hexagon_spritesheet_age_8.png
+magick montage $folder_path/{085..094}.png -tile 10x1 -geometry 120x120+5+5 -background 'none' $folder_path/base_hexagon_spritesheet_age_9.png
+magick montage $folder_path/{095..104}.png -tile 10x1 -geometry 120x120+5+5 -background 'none' $folder_path/base_hexagon_spritesheet_age_10.png
 
 magick montage $folder_path/base_hexagon_spritesheet_age_{1..10}.png -tile 1x10 -geometry +5+5 -background 'none' $folder_path/base_hexagon_spritesheet.png
 rm $folder_path/base_hexagon_spritesheet_age_{1..10}.png
+
+folder_path="../special_achievement_icons"
+magick montage $folder_path/{empire,monument,universe,wonder,world}.png -tile 5x1 -geometry +5+5 -background 'none' $folder_path/special_achievement_icons.png
