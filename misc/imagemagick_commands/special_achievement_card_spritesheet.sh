d57c29078a356555d6fbf4779f5aa08d445521df
Micah Stairs <micah.stairs@gmail.com>
2021-10-14 15:49:03 -0400

    Clean up image files a bit.

--- misc/imagemagick_commands/special_achievement_card_spritesheet.sh
+++ misc/imagemagick_commands/special_achievement_card_spritesheet.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 folder_path="../card_backgrounds"
-magick montage $folder_path/Print_BaseCards_front-{106..110}.png  $folder_path/action_{1,2}.png -tile 7x1 -geometry +5+5 -background 'white' $folder_path/special_achievement_card_spritesheet.png
+magick montage $folder_path/special_achievement_{1..5}.png  $folder_path/action_{1,2}.png -tile 7x1 -geometry +5+5 -background 'white' $folder_path/special_achievement_card_spritesheet.png
 
