4bf89474b630714a57d033ae3805b9ca32c212db
Micah Stairs <micah.stairs@gmail.com>
2021-10-22 11:03:56 -0400

    Remove unneeded special achievement images.

--- misc/imagemagick_commands/special_achievement_icon_spritesheet.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-folder_path="../special_achievement_icons"
-magick montage $folder_path/{empire,monument,universe,wonder,world}.png -tile 5x1 -geometry +5+5 -background 'none' $folder_path/special_achievement_icons.png
