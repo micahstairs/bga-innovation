a3a1fcf9a308795aff3a769e97c2eb22f38247c5
Chris Oelmueller <coelmueller@seibert-media.net>
2021-10-24 19:54:18 +0200

    Script to generate colored border images on white background
    Improves #62

--- misc/imagemagick_commands/bordered_square_icons.sh
+++ misc/imagemagick_commands/bordered_square_icons.sh
@@ -5,6 +5,21 @@ ICON_SRC_PATH="../square_icons"
 BORDER_SRC_PATH="../card_icon_borders"
 RESULT_PATH="../square_icons/bordered"
 
+# (1) Generate colored square borders set on white background
+function generate_border()
+{
+    local border_color=${1}  # e.g. blue
+    local border_hexcolor=${2}  # e.g. '#0e2a50'
+    local result_img="${BORDER_SRC_PATH}/${border_color}_square_icon_border.png"
+    magick convert -size '155x155' 'canvas:white' -fill 'white' -stroke $border_hexcolor -strokewidth 6 -draw 'rectangle 2,2 152,152' $result_img
+}
+generate_border 'blue' '#0e2a50'
+generate_border 'red' '#5a090c'
+generate_border 'green' '#122c18'
+generate_border 'yellow' '#8b6a28'
+generate_border 'purple' '#220e40'
+
+# (2) Generate each combination of icon + colored border box
 function borderize()
 {
     local icon_type=${1}  # e.g. crown leaf lightbulb castle factory clock
@@ -12,8 +27,7 @@ function borderize()
     local icon_img="${ICON_SRC_PATH}/${icon_type}.png"
     local border_img="${BORDER_SRC_PATH}/${border_color}_square_icon_border.png"
     local result_img="${RESULT_PATH}/${icon_type}_${border_color}.png"
-    # -1-1: there are subpixel problems with rendering artifacts => shift centered content by 1px towards top+left
-    magick convert -composite -gravity center -geometry -1-1 $border_img $icon_img $result_img
+    magick convert -composite -gravity center $border_img $icon_img $result_img
 }
 
 for icon in {crown,leaf,lightbulb,castle,factory,clock}; do
@@ -22,11 +36,12 @@ for icon in {crown,leaf,lightbulb,castle,factory,clock}; do
     done
 done
 
+# (3) Generate spritesheet from all individual bordered icons constructed in step 2
 magick montage \
     "${RESULT_PATH}/"{crown,leaf,lightbulb,castle,factory,clock}"_blue.png" \
     "${RESULT_PATH}/"{crown,leaf,lightbulb,castle,factory,clock}"_red.png" \
     "${RESULT_PATH}/"{crown,leaf,lightbulb,castle,factory,clock}"_green.png" \
     "${RESULT_PATH}/"{crown,leaf,lightbulb,castle,factory,clock}"_yellow.png" \
     "${RESULT_PATH}/"{crown,leaf,lightbulb,castle,factory,clock}"_purple.png" \
--tile 6x5 -geometry +2+2 -background 'none' "${RESULT_PATH}/bordered_square_icons_spritesheet.png"
+-tile 6x5 -geometry +2+2 -background 'none' "${RESULT_PATH}/../bordered_square_icons_spritesheet.png"
 
