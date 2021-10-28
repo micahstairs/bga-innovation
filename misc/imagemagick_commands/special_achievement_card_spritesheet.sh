084e4dca8a0c72ba6be89dc9f646233b2f0cf3cf
normaljosh <normaljosh@gmail.com>
2021-10-28 06:41:30 -0500

    adjust crop offsets for spritesheet

--- misc/imagemagick_commands/special_achievement_card_spritesheet.sh
+++ misc/imagemagick_commands/special_achievement_card_spritesheet.sh
@@ -8,11 +8,11 @@ wonder_num=3
 world_num=4
 universe_num=5
 
-empire_offset="+145+190"
-monument_offset="+145+210"
+empire_offset="+150+180"
+monument_offset="+150+200"
 wonder_offset="+150+150"
-world_offset="+145+160"
-universe_offset="+145+190"
+world_offset="+150+160"
+universe_offset="+150+190"
 
 magick convert "$folder_path/base_special_achievement_${empire_num}.png" -crop 450x330${empire_offset} +repage  "$folder_path/base_special_achievement_${empire_num}_cropped.png"
 magick convert "$folder_path/base_special_achievement_${monument_num}.png" -crop 450x330${monument_offset} +repage  "$folder_path/base_special_achievement_${monument_num}_cropped.png"
@@ -27,14 +27,15 @@ history_num=3
 supremacy_num=4
 wealth_num=5
 
-destiny_offset="+145+190"
-heritage_offset="+145+210"
-history_offset="+150+150"
-supremacy_offset="+145+160"
-wealth_offset="+145+190"
+destiny_offset="+150+155"
+#heritage also needed resize
+heritage_offset="+102+120"
+history_offset="+150+140"
+supremacy_offset="+150+125"
+wealth_offset="+150+170"
 
 magick convert "$folder_path/echoes_special_achievement_${destiny_num}.png" -crop 450x330${destiny_offset} +repage  "$folder_path/echoes_special_achievement_${destiny_num}_cropped.png"
-magick convert "$folder_path/echoes_special_achievement_${heritage_num}.png" -crop 450x330${heritage_offset} +repage  "$folder_path/echoes_special_achievement_${heritage_num}_cropped.png"
+magick convert "$folder_path/echoes_special_achievement_${heritage_num}.png" -crop 545x400${heritage_offset} +repage  -resize 450x330 "$folder_path/echoes_special_achievement_${heritage_num}_cropped.png"
 magick convert "$folder_path/echoes_special_achievement_${history_num}.png" -crop 450x330${history_offset} +repage  "$folder_path/echoes_special_achievement_${history_num}_cropped.png"
 magick convert "$folder_path/echoes_special_achievement_${supremacy_num}.png" -crop 450x330${supremacy_offset} +repage  "$folder_path/echoes_special_achievement_${supremacy_num}_cropped.png"
 magick convert "$folder_path/echoes_special_achievement_${wealth_num}.png" -crop 450x330${wealth_offset} +repage  "$folder_path/echoes_special_achievement_${wealth_num}_cropped.png"
@@ -46,11 +47,11 @@ rivalry_num=3
 trade_num=4
 war_num=5
 
-advancement_offset="+145+190"
-expansion_offset="+145+210"
-rivalry_offset="+150+150"
-trade_offset="+145+160"
-war_offset="+145+190"
+advancement_offset="+150+110"
+expansion_offset="+150+160"
+rivalry_offset="+150+130"
+trade_offset="+150+185"
+war_offset="+150+115"
 
 magick convert "$folder_path/figures_special_achievement_${advancement_num}.png" -crop 450x330${advancement_offset} +repage  "$folder_path/figures_special_achievement_${advancement_num}_cropped.png"
 magick convert "$folder_path/figures_special_achievement_${expansion_num}.png" -crop 450x330${expansion_offset} +repage  "$folder_path/figures_special_achievement_${expansion_num}_cropped.png"
@@ -65,15 +66,16 @@ legend_num=3
 repute_num=4
 victory_num=5
 
-fame_offset="+145+190"
-glory_offset="+145+210"
-legend_offset="+150+150"
-repute_offset="+145+160"
-victory_offset="+145+190"
+fame_offset="+150+120"
+glory_offset="+150+170"
+#legend also needed resize
+legend_offset="+102+100"
+repute_offset="+150+140"
+victory_offset="+150+135"
 
 magick convert "$folder_path/cities_special_achievement_${fame_num}.png" -crop 450x330${fame_offset} +repage  "$folder_path/cities_special_achievement_${fame_num}_cropped.png"
 magick convert "$folder_path/cities_special_achievement_${glory_num}.png" -crop 450x330${glory_offset} +repage  "$folder_path/cities_special_achievement_${glory_num}_cropped.png"
-magick convert "$folder_path/cities_special_achievement_${legend_num}.png" -crop 450x330${legend_offset} +repage  "$folder_path/cities_special_achievement_${legend_num}_cropped.png"
+magick convert "$folder_path/cities_special_achievement_${legend_num}.png" -crop 545x400${legend_offset} +repage  -resize 450x330 "$folder_path/cities_special_achievement_${legend_num}_cropped.png"
 magick convert "$folder_path/cities_special_achievement_${repute_num}.png" -crop 450x330${repute_offset} +repage  "$folder_path/cities_special_achievement_${repute_num}_cropped.png"
 magick convert "$folder_path/cities_special_achievement_${victory_num}.png" -crop 450x330${victory_offset} +repage  "$folder_path/cities_special_achievement_${victory_num}_cropped.png"
 
@@ -94,4 +96,4 @@ done
 
 magick montage $folder_path/base_special_achievement_{1..5}_cropped_rotated.png  $folder_path/echoes_special_achievement_{1..5}_cropped_rotated.png $folder_path/figures_special_achievement_{1..5}_cropped_rotated.png $folder_path/cities_special_achievement_{1..5}_cropped_rotated.png -tile 5x4 -geometry +5+5 -background 'white' $folder_path/special_achievement_card_rotated_spritesheet.png
 rm $folder_path/base_special_achievement_{1..5}_cropped_rotated.png  $folder_path/echoes_special_achievement_{1..5}_cropped_rotated.png $folder_path/figures_special_achievement_{1..5}_cropped_rotated.png $folder_path/cities_special_achievement_{1..5}_cropped_rotated.png 
-rm $folder_path/base_special_achievement_{1..5}_cropped.png  $folder_path/echoes_special_achievement_{1..5}_cropped.png $folder_path/figures_special_achievement_{1..5}_cropped.png
+rm $folder_path/base_special_achievement_{1..5}_cropped.png  $folder_path/echoes_special_achievement_{1..5}_cropped.png $folder_path/figures_special_achievement_{1..5}_cropped.png $folder_path/cities_special_achievement_{1..5}_cropped.png
