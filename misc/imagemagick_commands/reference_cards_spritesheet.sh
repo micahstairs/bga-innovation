f05fa36e029bac929491ce5ddb9a77aabd6c1213
Micah Stairs <micah.stairs@gmail.com>
2021-10-29 21:50:50 -0400

    Redirect reference card output.

--- misc/imagemagick_commands/reference_cards_spritesheet.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-folder_path="../reference_cards"
-magick montage $folder_path/{side_1_notext,side_2_notext,side_1}.png -tile 1x3 -geometry +5+5 -background 'none' $folder_path/reference_cards_spritesheet.png
