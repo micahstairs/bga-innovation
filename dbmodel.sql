2513f4860c29f62d56f71860517136c517335aa5
kahliadev <collin.m.spencer@gmail.com>
2021-10-31 12:36:45 -0400

    #73

--- dbmodel.sql
+++ dbmodel.sql
@@ -237,6 +237,7 @@ INSERT INTO `card` (`id`, `age`, `color`, `spot_1`, `spot_2`, `spot_3`, `spot_4`
 (111, 1, 0, 4, 4, 4, 0, 4, 0, 'deck', 0, NULL, FALSE),
 (112, 1, 0, 1, 0, 2, 2, 2, 0, 'deck', 0, NULL, FALSE),
 (113, 1, 1, 4, 2, 0, 2, 2, 0, 'deck', 0, NULL, FALSE),
+(114, 1, 1, 2, 2, 2, 0, 2, 0, 'deck', 4, NULL, FALSE),
 (115, 1, 1, 0, 1, 4, 1, 1, 0, 'deck', 0, NULL, FALSE),
 (119, 1, 3, 0, 3, 3, 3, 3, 0, 'deck', 0, NULL, FALSE),
 (120, 1, 3, 0, 1, 1, 1, 1, 0, 'deck', 0, NULL, FALSE),
