4fe526a31280069985ded3a8cdf53ac094d2c2c5
Micah Stairs <micah.stairs@gmail.com>
2021-11-05 21:03:16 -0400

    Directly reference source card backs instead of keeping a duplicate file.

--- misc/imagemagick_commands/card_backs_portrait.sh
+++ misc/imagemagick_commands/card_backs_portrait.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 folder_path="../card_backgrounds"
+mkdir temp
 
-mkdir $folder_path/temp
-
-mogrify -path $folder_path/temp -resize 110x150 \
-    $folder_path/age_{1..10}_base.png $folder_path/relic_base.png \
-    $folder_path/age_{1..10}_artifacts.png $folder_path/relic_artifacts.png \
-    $folder_path/age_{1..10}_cities.png $folder_path/relic_cities.png \
-    $folder_path/age_{1..10}_echoes.png $folder_path/relic_echoes.png \
-    $folder_path/age_{1..10}_figures.png $folder_path/relic_figures.png
+mogrify -path temp -resize 110x150 \
+    ../cards/Print_BaseCards_back/Print_BaseCards_back-0{0..9}6.png ../cards/Print_ArtifactsCards_back/Print_ArtifactsCards_back-106.png \
+    ../cards/Print_ArtifactsCards_back/Print_ArtifactsCards_back-0{0..9}6.png ../cards/Print_ArtifactsCards_back/Print_ArtifactsCards_back-107.png \
+    ../cards/Print_CitiesCards_back/Print_CitiesCards_back-0{0..9}6.png ../cards/Print_ArtifactsCards_back/Print_ArtifactsCards_back-108.png \
+    ../cards/Print_EchoesCards_back/Print_EchoesCards_back-0{0..9}6.png ../cards/Print_ArtifactsCards_back/Print_ArtifactsCards_back-109.png \
+    ../cards/Print_FiguresCards_back/Print_FiguresCards_back-0{0..9}6.png ../cards/Print_ArtifactsCards_back/Print_ArtifactsCards_back-110.png
 
 magick montage \
-    $folder_path/temp/age_{1..10}_base.png $folder_path/temp/relic_base.png \
-    $folder_path/temp/age_{1..10}_artifacts.png $folder_path/temp/relic_artifacts.png \
-    $folder_path/temp/age_{1..10}_cities.png $folder_path/temp/relic_cities.png \
-    $folder_path/temp/age_{1..10}_echoes.png $folder_path/temp/relic_echoes.png \
-    $folder_path/temp/age_{1..10}_figures.png $folder_path/temp/relic_figures.png \
+    temp/Print_BaseCards_back-0{0..9}6.png temp/Print_ArtifactsCards_back-106.png \
+    temp/Print_ArtifactsCards_back-0{0..9}6.png temp/Print_ArtifactsCards_back-107.png \
+    temp/Print_CitiesCards_back-0{0..9}6.png temp/Print_ArtifactsCards_back-108.png \
+    temp/Print_EchoesCards_back-0{0..9}6.png temp/Print_ArtifactsCards_back-109.png \
+    temp/Print_FiguresCards_back-0{0..9}6.png temp/Print_ArtifactsCards_back-110.png \
 -tile 11x5 -geometry +3+3 -background 'none' ../../img/card_backs_portrait.jpg
 
-rm -r $folder_path/temp
+rm -r temp
