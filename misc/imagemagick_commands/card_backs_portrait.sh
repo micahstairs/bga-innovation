70550143d80c615f7403113e56664e8a1a51907e
Micah Stairs <micah.stairs@gmail.com>
2021-11-05 20:45:49 -0400

    Reorder spritesheet rows so that it is in order of anticipcated implementation.

--- misc/imagemagick_commands/card_backs_portrait.sh
+++ misc/imagemagick_commands/card_backs_portrait.sh
@@ -5,17 +5,17 @@ mkdir $folder_path/temp
 
 mogrify -path $folder_path/temp -resize 110x150 \
     $folder_path/age_{1..10}_base.png $folder_path/relic_base.png \
-    $folder_path/age_{1..10}_echoes.png $folder_path/relic_echoes.png \
-    $folder_path/age_{1..10}_figures.png $folder_path/relic_figures.png \
+    $folder_path/age_{1..10}_artifacts.png $folder_path/relic_artifacts.png \
     $folder_path/age_{1..10}_cities.png $folder_path/relic_cities.png \
-    $folder_path/age_{1..10}_artifacts.png $folder_path/relic_artifacts.png
+    $folder_path/age_{1..10}_echoes.png $folder_path/relic_echoes.png \
+    $folder_path/age_{1..10}_figures.png $folder_path/relic_figures.png
 
 magick montage \
     $folder_path/temp/age_{1..10}_base.png $folder_path/temp/relic_base.png \
+    $folder_path/temp/age_{1..10}_artifacts.png $folder_path/temp/relic_artifacts.png \
+    $folder_path/temp/age_{1..10}_cities.png $folder_path/temp/relic_cities.png \
     $folder_path/temp/age_{1..10}_echoes.png $folder_path/temp/relic_echoes.png \
     $folder_path/temp/age_{1..10}_figures.png $folder_path/temp/relic_figures.png \
-    $folder_path/temp/age_{1..10}_cities.png $folder_path/temp/relic_cities.png \
-    $folder_path/temp/age_{1..10}_artifacts.png $folder_path/temp/relic_artifacts.png \
 -tile 11x5 -geometry +3+3 -background 'none' ../../img/card_backs_portrait.jpg
 
 rm -r $folder_path/temp
--- misc/imagemagick_commands/special_achievements.sh
+++ misc/imagemagick_commands/special_achievements.sh
@@ -25,6 +25,26 @@ magick convert "$folder_path/base_special_achievement_${wonder_num}.png" -crop 4
 magick convert "$folder_path/base_special_achievement_${world_num}.png" -crop 450x330${world_offset} +repage  "$folder_path/temp/base_special_achievement_${world_num}_cropped.png"
 magick convert "$folder_path/base_special_achievement_${universe_num}.png" -crop 450x330${universe_offset} +repage  "$folder_path/temp/base_special_achievement_${universe_num}_cropped.png"
 
+# CITIES
+
+fame_num=1
+glory_num=2
+legend_num=3
+repute_num=4
+victory_num=5
+
+fame_offset="+150+120"
+glory_offset="+150+170"
+legend_offset="+102+100"
+repute_offset="+150+140"
+victory_offset="+150+135"
+
+magick convert "$folder_path/cities_special_achievement_${fame_num}.png" -crop 450x330${fame_offset} +repage  "$folder_path/temp/cities_special_achievement_${fame_num}_cropped.png"
+magick convert "$folder_path/cities_special_achievement_${glory_num}.png" -crop 450x330${glory_offset} +repage  "$folder_path/temp/cities_special_achievement_${glory_num}_cropped.png"
+magick convert "$folder_path/cities_special_achievement_${legend_num}.png" -crop 545x400${legend_offset} +repage  -resize 450x330 "$folder_path/temp/cities_special_achievement_${legend_num}_cropped.png"
+magick convert "$folder_path/cities_special_achievement_${repute_num}.png" -crop 450x330${repute_offset} +repage  "$folder_path/temp/cities_special_achievement_${repute_num}_cropped.png"
+magick convert "$folder_path/cities_special_achievement_${victory_num}.png" -crop 450x330${victory_offset} +repage  "$folder_path/temp/cities_special_achievement_${victory_num}_cropped.png"
+
 # ECHOES
 
 destiny_num=1
@@ -65,39 +85,19 @@ magick convert "$folder_path/figures_special_achievement_${rivalry_num}.png" -cr
 magick convert "$folder_path/figures_special_achievement_${trade_num}.png" -crop 450x330${trade_offset} +repage  "$folder_path/temp/figures_special_achievement_${trade_num}_cropped.png"
 magick convert "$folder_path/figures_special_achievement_${war_num}.png" -crop 450x330${war_offset} +repage  "$folder_path/temp/figures_special_achievement_${war_num}_cropped.png"
 
-# CITIES
-
-fame_num=1
-glory_num=2
-legend_num=3
-repute_num=4
-victory_num=5
-
-fame_offset="+150+120"
-glory_offset="+150+170"
-legend_offset="+102+100"
-repute_offset="+150+140"
-victory_offset="+150+135"
-
-magick convert "$folder_path/cities_special_achievement_${fame_num}.png" -crop 450x330${fame_offset} +repage  "$folder_path/temp/cities_special_achievement_${fame_num}_cropped.png"
-magick convert "$folder_path/cities_special_achievement_${glory_num}.png" -crop 450x330${glory_offset} +repage  "$folder_path/temp/cities_special_achievement_${glory_num}_cropped.png"
-magick convert "$folder_path/cities_special_achievement_${legend_num}.png" -crop 545x400${legend_offset} +repage  -resize 450x330 "$folder_path/temp/cities_special_achievement_${legend_num}_cropped.png"
-magick convert "$folder_path/cities_special_achievement_${repute_num}.png" -crop 450x330${repute_offset} +repage  "$folder_path/temp/cities_special_achievement_${repute_num}_cropped.png"
-magick convert "$folder_path/cities_special_achievement_${victory_num}.png" -crop 450x330${victory_offset} +repage  "$folder_path/temp/cities_special_achievement_${victory_num}_cropped.png"
-
 # Shrink images
 mogrify -path $folder_path/temp/scaled -resize 150x110 \
     $folder_path/temp/base_special_achievement_{1..5}_cropped.png \
+    $folder_path/temp/cities_special_achievement_{1..5}_cropped.png \
     $folder_path/temp/echoes_special_achievement_{1..5}_cropped.png \
-    $folder_path/temp/figures_special_achievement_{1..5}_cropped.png \
-    $folder_path/temp/cities_special_achievement_{1..5}_cropped.png
+    $folder_path/temp/figures_special_achievement_{1..5}_cropped.png
 
 # Make landscape spritesheet
 magick montage \
   $folder_path/temp/scaled/base_special_achievement_{1..5}_cropped.png \
+  $folder_path/temp/scaled/cities_special_achievement_{1..5}_cropped.png \
   $folder_path/temp/scaled/echoes_special_achievement_{1..5}_cropped.png \
   $folder_path/temp/scaled/figures_special_achievement_{1..5}_cropped.png \
-  $folder_path/temp/scaled/cities_special_achievement_{1..5}_cropped.png \
   -tile 5x4 -geometry +5+5 -background 'white' ../../img/special_achievements_landscape.jpg
 
 
@@ -105,17 +105,17 @@ magick montage \
 for i in {1..5}
 do 
     magick convert "$folder_path/temp/scaled/base_special_achievement_${i}_cropped.png" -rotate 90 "$folder_path/temp/scaled/base_special_achievement_${i}_cropped_rotated.png"
+    magick convert "$folder_path/temp/scaled/cities_special_achievement_${i}_cropped.png" -rotate 90 "$folder_path/temp/scaled/cities_special_achievement_${i}_cropped_rotated.png"
     magick convert "$folder_path/temp/scaled/echoes_special_achievement_${i}_cropped.png" -rotate 90 "$folder_path/temp/scaled/echoes_special_achievement_${i}_cropped_rotated.png"
     magick convert "$folder_path/temp/scaled/figures_special_achievement_${i}_cropped.png" -rotate 90 "$folder_path/temp/scaled/figures_special_achievement_${i}_cropped_rotated.png"
-    magick convert "$folder_path/temp/scaled/cities_special_achievement_${i}_cropped.png" -rotate 90 "$folder_path/temp/scaled/cities_special_achievement_${i}_cropped_rotated.png"
 done
 
 # Make portrait spritesheet
 magick montage \
   $folder_path/temp/scaled/base_special_achievement_{1..5}_cropped_rotated.png \
+  $folder_path/temp/scaled/cities_special_achievement_{1..5}_cropped_rotated.png \
   $folder_path/temp/scaled/echoes_special_achievement_{1..5}_cropped_rotated.png \
   $folder_path/temp/scaled/figures_special_achievement_{1..5}_cropped_rotated.png \
-  $folder_path/temp/scaled/cities_special_achievement_{1..5}_cropped_rotated.png \
   -tile 5x4 -geometry +5+5 -background 'white' ../../img/special_achievements_portrait.jpg
 
 # Clean up
