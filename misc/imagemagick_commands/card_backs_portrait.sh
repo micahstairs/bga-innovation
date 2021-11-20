7d19c82dbbd5ce31134f30791ece4024ef1e2874
Micah Stairs <micah.stairs@gmail.com>
2021-11-19 21:57:35 -0500

    Fix order of relic images.

--- misc/imagemagick_commands/card_backs_portrait.sh
+++ misc/imagemagick_commands/card_backs_portrait.sh
@@ -3,16 +3,16 @@ mkdir temp
 
 mogrify -path temp -resize 110x150 \
     ../cards/Print_BaseCards_back/Print_BaseCards_back-0{0..9}6.png ../cards/Print_ArtifactsCards_back/Print_ArtifactsCards_back-106.png \
-    ../cards/Print_ArtifactsCards_back/Print_ArtifactsCards_back-0{0..9}6.png ../cards/Print_ArtifactsCards_back/Print_ArtifactsCards_back-107.png \
+    ../cards/Print_ArtifactsCards_back/Print_ArtifactsCards_back-0{0..9}6.png ../cards/Print_ArtifactsCards_back/Print_ArtifactsCards_back-109.png \
     ../cards/Print_CitiesCards_back/Print_CitiesCards_back-0{0..9}6.png ../cards/Print_ArtifactsCards_back/Print_ArtifactsCards_back-108.png \
-    ../cards/Print_EchoesCards_back/Print_EchoesCards_back-0{0..9}6.png ../cards/Print_ArtifactsCards_back/Print_ArtifactsCards_back-109.png \
+    ../cards/Print_EchoesCards_back/Print_EchoesCards_back-0{0..9}6.png ../cards/Print_ArtifactsCards_back/Print_ArtifactsCards_back-107.png \
     ../cards/Print_FiguresCards_back/Print_FiguresCards_back-0{0..9}6.png ../cards/Print_ArtifactsCards_back/Print_ArtifactsCards_back-110.png
 
 magick montage \
     temp/Print_BaseCards_back-0{0..9}6.png temp/Print_ArtifactsCards_back-106.png \
-    temp/Print_ArtifactsCards_back-0{0..9}6.png temp/Print_ArtifactsCards_back-107.png \
+    temp/Print_ArtifactsCards_back-0{0..9}6.png temp/Print_ArtifactsCards_back-109.png \
     temp/Print_CitiesCards_back-0{0..9}6.png temp/Print_ArtifactsCards_back-108.png \
-    temp/Print_EchoesCards_back-0{0..9}6.png temp/Print_ArtifactsCards_back-109.png \
+    temp/Print_EchoesCards_back-0{0..9}6.png temp/Print_ArtifactsCards_back-107.png \
     temp/Print_FiguresCards_back-0{0..9}6.png temp/Print_ArtifactsCards_back-110.png \
 -tile 11x5 -geometry +3+3 -background 'none' ../../img/card_backs_portrait.jpg
 
