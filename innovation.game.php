f4b188c0bf4ead99abf84cbf23bcc4c7e133bb02
Micah Stairs <micah.stairs@gmail.com>
2022-01-17 21:05:22 -0500

    Clean up code and add TODOs about card interpretation

--- innovation.game.php
+++ innovation.game.php
@@ -9520,23 +9520,22 @@ class Innovation extends Table
 
             // id 209, Artifacts age 10: Maastricht Treaty
             case "209N1":
-                // If you have the most cards in your score pile, you win.
-                $i_win = true;
-                $cards_in_my_score_pile = count(self::getCardsInLocation($player_id, 'score'));
+                // "If you have the most cards in your score pile, you win"
+                $win_condition_met = true;
+                $cards_in_my_score_pile = self::countCardsInLocation($player_id, 'score');
                 $players = self::loadPlayersBasicInfos();
-                foreach($players as $any_player_id => $player) {
-                    if ($player_id != $any_player_id && count(self::getCardsInLocation($any_player_id, 'score')) >= $cards_in_my_score_pile) {
-                        $i_win = false;
+                foreach ($players as $any_player_id => $player) {
+                    if ($player_id != $any_player_id && self::countCardsInLocation($any_player_id, 'score') >= $cards_in_my_score_pile) {
+                        $win_condition_met = false;
                     }
                 }
-                if ($i_win) {
+                if ($win_condition_met) {
                     self::notifyPlayer($player_id, 'log', clienttranslate('${You} have the most cards in your score pile.'), array('You' => 'You'));
                     self::notifyAllPlayersBut($player_id, 'log', clienttranslate('${player_name} has the most cards in their score pile.'), array('player_name' => self::getColoredText(self::getPlayerNameFromId($player_id), $player_id)));
                     self::setGameStateValue('winner_by_dogma', $player_id);
                     self::trace('EOG bubbled from self::stPlayerInvolvedTurn Maastricht Treaty');
                     throw new EndOfGame();
-                }
-                else {
+                } else {
                     self::notifyPlayer($player_id, 'log', clienttranslate('${You} do not have the most cards in your score pile.'), array('You' => 'You'));
                     self::notifyAllPlayersBut($player_id, 'log', clienttranslate('${player_name} does not have the most cards in their score pile.'), array('player_name' => self::getColoredText(self::getPlayerNameFromId($player_id), $player_id)));
                 }
@@ -9544,37 +9543,39 @@ class Innovation extends Table
 
             // id 210, Artifacts age 10: Seikan Tunnel
             case "210N1":
-                // If you have the most cards of a color showing on your board out of all colors on all boards, you win.
-                $i_win = true;
+                // "If you have the most cards of a color showing on your board out of all colors on all boards, you win"
+                $win_condition_met = true;
                 // Get all of the visible card counts
-                $vis_cards_on_board = array();
+                $visible_card_counts = array();
                 for($color = 0; $color < 5; $color++){
-                    $vis_cards_on_board[] = self::countVisibleCards($player_id, $color);
+                    $visible_card_counts[] = self::countVisibleCards($player_id, $color);
                 }
                 
                 // See if the max happens more than once  
-                $most_vis_cards_on_board = max($vis_cards_on_board);
-                foreach (array_count_values($vis_cards_on_board) as $value => $count) {
-                    if ($count > 1 && $value == $most_vis_cards_on_board) {
-                        $i_win = false;
+                // TODO: Waiting for clarification on this: https://boardgamegeek.com/thread/1937834/seikan-tunnel-artifact-rules-question.
+                $max_visible_card_count = max($visible_card_counts);
+                foreach (array_count_values($visible_cards_on_board) as $value => $count) {
+                    if ($count > 1 && $value == $max_visible_card_count) {
+                        $win_condition_met = false;
                         self::notifyPlayer($player_id, 'log', clienttranslate('${You} have multiple piles with the same maximum number of visible cards in a pile.'), array('You' => 'You'));
                     }
                 }
-                if ($i_win) {
+
+                if ($win_condition_met) {
                     $players = self::loadPlayersBasicInfos();
-                    foreach($players as $any_player_id => $player) {
-                        if ($player_id != $any_player_id)
-                        {
-                            for($color = 0; $color < 5; $color++){
-                                if ($most_vis_cards_on_board <= self::countVisibleCards($any_player_id, $color)) {
-                                    $i_win = false;
+                    foreach ($players as $any_player_id => $player) {
+                        if ($player_id != $any_player_id) {
+                            for ($color = 0; $color < 5; $color++){
+                                if ($max_visible_card_count <= self::countVisibleCards($any_player_id, $color)) {
+                                    $win_condition_met = false;
                                 }
                             }
                         }
                     }
                     
                     // There is a single pile on your board that has the most cards of all piles on all boards
-                    if ($i_win) {
+                    // TODO: Revise wording once we get a clarification on https://boardgamegeek.com/thread/1937834/seikan-tunnel-artifact-rules-question.
+                    if ($win_condition_met) {
                         self::notifyPlayer($player_id, 'log', clienttranslate('${You} have the most visible cards in a pile.'), array('You' => 'You'));
                         self::notifyAllPlayersBut($player_id, 'log', clienttranslate('${player_name} has the most visible cards in a pile.'), array('player_name' => self::getColoredText(self::getPlayerNameFromId($player_id), $player_id)));
                         self::setGameStateValue('winner_by_dogma', $player_id);
