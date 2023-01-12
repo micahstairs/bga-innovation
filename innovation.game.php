003f996733d13058950a5519d1a2f27126291dc8
Micah Stairs <micah.stairs@gmail.com>
2023-01-12 09:34:47 -0500

    Fix upgradeTableDb

--- innovation.game.php
+++ innovation.game.php
@@ -122,6 +122,9 @@ class Innovation extends Table
     // TODO(CITIES): Test this by simulating table migrations.
     function upgradeTableDb($from_version) {
         // TODO(CITIES): Before launching to production, update the release that we are comparing it to.
+        if (is_null(self::getUniqueValueFromDB("SHOW COLUMNS FROM `card` LIKE 'democracy_counter'"))) {
+            self::applyDbUpgradeToAllDB("ALTER TABLE DBPREFIX_card ADD `democracy_counter` TINYINT UNSIGNED NOT NULL DEFAULT 0;");
+        }
         if ($from_version <= 2212252101) {
             self::initGameStateLabels(array(
                 'endorse_action_state' => 19,
