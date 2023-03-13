74693f3f592b6bd9374b05061fbc4c4f454374a0
Micah Stairs <micah.stairs@gmail.com>
2023-03-13 04:38:11 -0400

    Fix merge conflict

--- innovation.game.php
+++ innovation.game.php
@@ -2265,12 +2265,7 @@ class Innovation extends Table
                 $message_for_others = clienttranslate('${player_name} achieves a ${<}${age}${>}.');
             }
             break;
-<<<<<<< HEAD
         case 'achievements->junk':
-            $message_for_player = clienttranslate('${You} junk a ${<}${age}${>} from the available achievements.');
-            $message_for_others = clienttranslate('${player_name} junks a ${<}${age}${>} from the available achievements.');
-=======
-        case 'achievements->removed':
             if ($card['age'] === null) { // Special achievement
                 $message_for_player = clienttranslate('${You} junk ${<<<}${name}${>>>} from the available achievements.');
                 $message_for_others = clienttranslate('${player_name} junks ${<<<}${name}${>>>} from the available achievements.');
@@ -2278,7 +2273,6 @@ class Innovation extends Table
                 $message_for_player = clienttranslate('${You} junk a ${<}${age}${>} from the available achievements.');
                 $message_for_others = clienttranslate('${player_name} junks a ${<}${age}${>} from the available achievements.');
             }
->>>>>>> 6ac8ce3 (#971 - ages 1-3)
             break;
         case 'fountains->achievements':
             $message_for_player = clienttranslate('A fountain became visible on ${your} board so it now counts as an achievement.');
