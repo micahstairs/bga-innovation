dccae9f47cb70503a8a1b8aeca841ad27796789c
Micah Stairs <micah.stairs@gmail.com>
2023-06-03 21:47:07 -0400

    Try to get things work on CI

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,11 @@
 
 use BGAWorkbench\Test\StubProductionEnvironment;
 
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', 1);
+
+$_ENV['TEST'] = '1';
+
 // We're on GitHub Actions, so do this statically since the directory structure there is different
 $ci = getenv('CI');
 if (!empty($ci)) {
@@ -15,8 +20,6 @@ if (!empty($ci)) {
 define('BGA_GAME_NAME', $gameName);
 define('BGA_GAME_CLASS', ucfirst($gameName));
 
-error_reporting(E_ALL | E_STRICT);
-ini_set('display_errors', 1);
 $vendorDir = __DIR__ . '/../vendor';
 require_once $vendorDir . '/autoload.php';
 require_once __DIR__ . '/../modules/Innovation/GameInterface.php';
