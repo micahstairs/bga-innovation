68dacde52a84ca143a47dce1a7d0129944369ca3
Micah Stairs <micah.stairs@gmail.com>
2023-06-12 07:40:50 -0400

    Review El Dorado

--- modules/Innovation/Cards/Base/Card520.php
+++ modules/Innovation/Cards/Base/Card520.php
@@ -5,44 +5,33 @@ namespace Innovation\Cards\Base;
 use Innovation\Cards\Card;
 use Innovation\Cards\ExecutionState;
 
-class Card520 extends Card {
+class Card520 extends Card
+{
 
   // El Dorado:
-  //   - Draw and meld a 3, a 2, and a 1. If all three 
-  //     have a PROSPERITY, score all cards in the 5 deck. If two or 
-  //     more have a PROSPERITY, splay your green and blue cards right.
+  //   - Draw and meld a [3], a [2], and a [1]. If all three have a PROSPERITY, score all cards in
+  //     the [5] deck. If two or more have a PROSPERITY, splay your green and blue cards right.
 
-  public function initialExecution(ExecutionState $state) {
-    switch ($state->getEffectNumber()) {
-      case 1:
-        $card_1 = $this->game->executeDrawAndMeld($state->getPlayerId(), 3);
-        $card_2 = $this->game->executeDrawAndMeld($state->getPlayerId(), 2);
-        $card_3 = $this->game->executeDrawAndMeld($state->getPlayerId(), 1);
-        $cards_with_crowns_cnt = 0;
-        if ($this->game->hasRessource($card_1, 1)) {
-            $cards_with_crowns_cnt++;
-        }            
-        if ($this->game->hasRessource($card_2, 1)) {
-            $cards_with_crowns_cnt++;
+  public function initialExecution(ExecutionState $state)
+  {
+    $numCardsWithProsperityIcons = 0;
+    for ($i = 3; $i >= 1; $i--) {
+      $card = $this->game->executeDrawAndMeld($state->getPlayerId(), $i);
+      if ($this->game->hasRessource($card, \Innovation::PROSPERITY)) {
+        $numCardsWithProsperityIcons++;
+      }
+    }
+    if ($numCardsWithProsperityIcons == 3) {
+      $cardsInDeck = $this->game->getCardsInLocationKeyedByAge(0, 'deck');
+      foreach ($cardsInDeck[5] as $card) {
+        if ($card['type'] == \Innovation::BASE) {
+          $this->game->scoreCard($card, $state->getPlayerId());
         }
-        if ($this->game->hasRessource($card_3, 1)) {
-            $cards_with_crowns_cnt++;
-        }        
-        if ($cards_with_crowns_cnt == 3) {
-            $cards_in_decks = $this->game->getCardsInLocationKeyedByAge(0, 'deck');
-            foreach($cards_in_decks[5] as $card) {
-                if ($card['type'] == 0) { // don't score expansion cards
-                    $this->game->scoreCard($card, $state->getPlayerId());
-                }
-            }
-        }
-        if ($cards_with_crowns_cnt >= 2) {
-            $this->game->splayRight($state->getPlayerId(), $state->getPlayerId(), 2);
-            $this->game->splayRight($state->getPlayerId(), $state->getPlayerId(), 0);
-        }
-        
-        break;
+      }
+    }
+    if ($numCardsWithProsperityIcons >= 2) {
+      $this->game->splayRight($state->getPlayerId(), $state->getPlayerId(), \Innovation::GREEN);
+      $this->game->splayRight($state->getPlayerId(), $state->getPlayerId(), \Innovation::BLUE);
     }
   }
-
 }
