cd6615dd4c26faf33a5ba59df2711a461c160526
Micah Stairs <micah.stairs@gmail.com>
2023-06-27 23:13:04 -0400

    Implement Denver Airport (#1083)

---
+++ modules/Innovation/Cards/Unseen/Card580.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Innovation\Cards\Unseen;
+
+use Innovation\Cards\Card;
+
+class Card580 extends Card
+{
+
+  // Denver Airport:
+  //   - You may achieve one of your secrets regardless of eligibility.
+  //   - You may splay your purple cards up.
+
+  public function initialExecution()
+  {
+    self::setMaxSteps(1);
+  }
+
+  public function getInteractionOptions(): array
+  {
+    if (self::getEffectNumber() == 1) {
+      return [
+        'location_from' => 'safe',
+        'location_to'   => 'achievements',
+      ];
+    } else {
+      return [
+        'can_pass'        => true,
+        'splay_direction' => $this->game::UP,
+        'color'           => [$this->game::PURPLE],
+      ];
+    }
+  }
+
+}
