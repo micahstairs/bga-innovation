2ca9fac708f7ddd97a63ae123e5814344992c0cb
Micah Stairs <micah.stairs@gmail.com>
2023-07-01 16:06:04 -0400

    Implement Green Hydrogen (#1084)

---
+++ modules/Innovation/Cards/Unseen/Card589.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Innovation\Cards\Unseen;
+
+use Innovation\Cards\Card;
+
+class Card589 extends Card
+{
+
+  // Green Hydrogen:
+  //   - Score all non-top green cards on your board. Draw and tuck an [11] for each card scored.
+
+  public function initialExecution()
+  {
+    $greenCards = $this->game->getCardsInLocationKeyedByColor(self::getPlayerId(), 'board')[$this->game::GREEN];
+    $topGreenCard = self::getTopCardOfColor($this->game::GREEN);
+    $numScored = 0;
+    foreach ($greenCards as $card) {
+      if (!$card['id'] !== $topGreenCard['id']) {
+        self::score($card);
+        $numScored++;
+      }
+    }
+    for ($i = 0; $i < $numScored; $i++) {
+      self::drawAndTuck(11);
+    }
+  }
+
+}
