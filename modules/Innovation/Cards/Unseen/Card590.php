3a3fb00ba18ea1f9490729ab94a95a0997ca932f
Micah Stairs <micah.stairs@gmail.com>
2023-07-01 16:08:46 -0400

    Implement Mystery Box (#1084)

---
+++ modules/Innovation/Cards/Unseen/Card590.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Innovation\Cards\Unseen;
+
+use Innovation\Cards\Card;
+
+class Card590 extends Card
+{
+
+  // Mystery Box:
+  //   - Claim an available standard achievement, regardless of eligibility. Self-execute it.
+
+  public function initialExecution()
+  {
+    self::setMaxSteps(1);
+  }
+
+  public function getInteractionOptions(): array
+  {
+    return [
+      'owner_from'    => 0,
+      'location_from' => 'achievements',
+      'owner_to'      => self::getPlayerId(),
+      'location_to'   => 'achievements',
+      'age_min'       => 1,
+      'age_max'       => 11,
+    ];
+  }
+
+  public function handleCardChoice(int $cardId)
+  {
+    self::selfExecute(self::getLastSelectedCard());
+  }
+
+}
