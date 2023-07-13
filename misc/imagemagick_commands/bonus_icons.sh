db40548f47f4668787ffa5a8e94259b502daf1e5
Micah Stairs <micah.stairs@gmail.com>
2023-07-13 13:52:54 -0300

    Add bonus 12 icon (#1191)

--- misc/imagemagick_commands/bonus_icons.sh
+++ misc/imagemagick_commands/bonus_icons.sh
@@ -2,36 +2,18 @@
 # Setup
 mkdir temp
 
-### ECHOES ###
-
-# Coordinates for circles
-# First coord pair is center, second is any point the radius away from the center
-TOP_LEFT="134,122 134,45"
-BOTTOM_LEFT="137,414 137,337"
-BOTTOM_CENTER="380,414 380,337"
-BOTTOM_RIGHT="615,414 615,337"
-
-# Coordinates for ellipses
-# First coord pair is center, second is xradius, yradius, third is start and end of arc in degrees
-TOP_LEFT_ELLIPSE="134,122 40,45 0,360"
-BOTTOM_LEFT_ELLIPSE="137,414 40,45 0,360"
-BOTTOM_CENTER_ELLIPSE="380,414 40,45 0,360"
-BOTTOM_RIGHT_ELLIPSE="615,414 40,45 0,360"
-
 # File paths/prefixes
-READ_PATH="../cards/Print_EchoesCards_front/Print_EchoesCards_front-"
+READ_PATH="../cards/4E_Cities/Cities"
 
 # Extracts shape from a base card and places in a colored border
 # Covers number with an ellipse based on circle background
 # arg1: input filename
 # arg2: position of shape in card
 # arg4: output filename
-# example: extract_circle "002" "$BOTTOM_RIGHT" "000"
-
 extract_circle()
 {
     magick "${READ_PATH}$1.png" \( +clone -fill Black -colorize 100 -fill White -draw "circle ${2}" \)  -alpha off -compose CopyOpacity -composite -trim +repage "temp/$3_circle.png"
-    magick "temp/$3_circle.png" -fill '%[pixel:p{105,75}]' -draw 'ellipse 80,80 35,45 0,360' "temp/$3_circle.png"
+    magick "temp/$3_circle.png" -fill '%[pixel:p{106,76}]' -draw 'ellipse 115,108 58,68 0,360' "temp/$3_circle.png"
 }
 
 extract_ellipse()
@@ -39,27 +21,28 @@ extract_ellipse()
     magick "${READ_PATH}$1.png" \( +clone -fill Black -colorize 100 -fill White -draw "ellipse ${2}" \) -alpha off -compose CopyOpacity -composite -trim +repage "temp/$3_ellipse.png"
 }
 
-echo "Extracting echoes circle icons..."
+echo "Extracting bonus icons..."
 
 # Extract blue, red, green, yellow, purple bonus circles
-extract_circle "013" "$BOTTOM_LEFT" "1"
-extract_circle "008" "$BOTTOM_LEFT"  "2"
-extract_circle "059" "$BOTTOM_LEFT"  "3"
-extract_circle "003" "$BOTTOM_LEFT"  "4"
-extract_circle "022" "$BOTTOM_LEFT"  "5"
+extract_circle "22" "164,587 81,506" "1"
+extract_circle "97" "164,587 81,506" "2"
+extract_circle "20" "164,587 81,506" "3"
+extract_circle "68" "164,587 81,506" "4"
+extract_circle "35" "164,587 81,506" "5"
 
-# Extract numbers 1-11
-extract_ellipse "002" "$BOTTOM_RIGHT_ELLIPSE" "1"
-extract_ellipse "003" "$BOTTOM_LEFT_ELLIPSE"  "2"
-extract_ellipse "028" "$BOTTOM_RIGHT_ELLIPSE"  "3"
-extract_ellipse "037" "$BOTTOM_RIGHT_ELLIPSE"  "4"
-extract_ellipse "048" "$BOTTOM_RIGHT_ELLIPSE"  "5"
-extract_ellipse "054" "$BOTTOM_RIGHT_ELLIPSE"  "6"
-extract_ellipse "058" "$BOTTOM_RIGHT_ELLIPSE"  "7"
-extract_ellipse "068" "$BOTTOM_RIGHT_ELLIPSE"  "8"
-extract_ellipse "086" "$BOTTOM_RIGHT_ELLIPSE"  "9"
-extract_ellipse "091" "$BOTTOM_RIGHT_ELLIPSE"  "10"
-extract_ellipse "104" "$BOTTOM_RIGHT_ELLIPSE"  "11"
+# Extract numbers 1-12
+extract_ellipse "2"   "882,585 57,68 0,360" "1"
+extract_ellipse "18"  "882,580 57,68 0,360" "2"
+extract_ellipse "30"  "882,580 57,68 0,360" "3"
+extract_ellipse "44"  "883,580 57,68 0,360" "4"
+extract_ellipse "46"  "883,580 57,68 0,360" "5"
+extract_ellipse "65"  "531,580 57,68 0,360" "6"
+extract_ellipse "59"  "880,578 57,68 0,360" "7"
+extract_ellipse "78"  "164,580 57,68 0,360" "8"
+extract_ellipse "77"  "530,580 57,68 0,360" "9"
+extract_ellipse "92"  "531,582 57,68 0,360" "10"
+extract_ellipse "110" "882,585 57,68 0,360" "11"
+extract_ellipse "111" "882,585 57,68 0,360" "12"
 
 # Build row of circular bonus icons
 magick montage \
@@ -68,14 +51,14 @@ temp/{1..5}_circle.png \
 
 # Build row of number ellipses
 magick montage \
-temp/{1..11}_ellipse.png \
--trim -tile 11x1 -geometry +5+5 -background 'none' temp/bonus_numbers.png
+temp/{1..12}_ellipse.png \
+-trim -tile 12x1 -geometry +5+5 -background 'none' temp/bonus_numbers.png
 
 # Combine all images into a single spritesheet.
 magick montage \
 temp/bonus_circles.png \
 temp/bonus_numbers.png \
--tile 1x2 -geometry +0+0 -background 'none' ../../img/circle_icons.png
+-tile 1x2 -geometry +0+0 -background 'none' ../../img/bonus_icons.png
 
 echo "Cleaning up..."
 
