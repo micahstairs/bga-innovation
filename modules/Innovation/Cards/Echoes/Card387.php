751009a71bc1d3b6a924a3cb7bcedb4a31495d5a
Micah Stairs <micah.stairs@gmail.com>
2023-07-31 21:20:00 -0400

    Format file

--- modules/Innovation/Cards/Echoes/Card387.php
+++ modules/Innovation/Cards/Echoes/Card387.php
@@ -30,12 +30,12 @@ class Card387 extends Card
         self::setMaxSteps(1);
       }
     } else {
-      for ($color = 0; $color < 5 ; $color++) {
+      for ($color = 0; $color < 5; $color++) {
         if ($this->game->countVisibleIconsInPile(self::getPlayerId(), $this->game::HEX_IMAGE, $color) >= 5) {
-            $this->game->claimSpecialAchievement(self::getPlayerId(), 437);
-            break;
+          $this->game->claimSpecialAchievement(self::getPlayerId(), 437);
+          break;
         }
-    }
+      }
     }
   }
 
@@ -45,22 +45,20 @@ class Card387 extends Card
       return [
         'location_from' => 'board',
         'score_keyword' => true,
-        'age' => $this->game->getMinAgeOnBoardTopCards(self::getPlayerId()),
+        'age'           => $this->game->getMinAgeOnBoardTopCards(self::getPlayerId()),
       ];
-    } else if (self::isFirstNonDemand()) {
-      if (self::getCurrentStep() === 1) {
+    } else if (self::getCurrentStep() === 1) {
       return [
-        'can_pass' => true,
-        'location_from' => 'score',
+        'can_pass'       => true,
+        'location_from'  => 'score',
         'return_keyword' => true,
       ];
     } else {
       return [
-        'location_from' => 'score',
-        'return_keyword' => true,
+        'location_from'                   => 'score',
+        'return_keyword'                  => true,
         'card_ids_are_in_auxiliary_array' => true,
       ];
-    }
     }
   }
 
@@ -68,19 +66,19 @@ class Card387 extends Card
   {
     if (self::isFirstNonDemand()) {
       if (self::getCurrentStep() === 1) {
-      $cardIds = [];
-      foreach (self::getCards('score') as $scoreCard) {
+        $cardIds = [];
+        foreach (self::getCards('score') as $scoreCard) {
           if ($scoreCard['age'] != $card['age']) {
-              $cardIds[] = $scoreCard['id'];
+            $cardIds[] = $scoreCard['id'];
           }
+        }
+        self::setAuxiliaryArray($cardIds);
+        self::setMaxSteps(2);
+      } else {
+        self::drawAndTuck(6);
+        self::drawAndTuck(6);
+        self::drawAndTuck(6);
       }
-      self::setAuxiliaryArray($cardIds);
-      self::setMaxSteps(2);
-    } else {
-      self::drawAndTuck(6);
-      self::drawAndTuck(6);
-      self::drawAndTuck(6);
-    }
     }
   }
 
