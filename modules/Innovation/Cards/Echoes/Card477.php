4f86780b55ae08c1a3919d113e626a784b8acb5c
Micah Stairs <micah.stairs@gmail.com>
2023-08-16 22:11:51 -0400

    Implement Esports (#1265)

---
+++ modules/Innovation/Cards/Echoes/Card477.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Innovation\Cards\Echoes;
+
+use Innovation\Cards\Card;
+
+class Card477 extends Card
+{
+
+  // Esports
+  //   - For each non-yellow top card on your board, draw and score a card of equal value, in
+  //     ascending order. If you do, and Esports was foreseen, you win.
+
+  public function initialExecution()
+  {
+    $values = [];
+    foreach (self::getAllColorsOtherThan($this->game::YELLOW) as $color) {
+      $topCard = self::getTopCardOfColor($color);
+      if ($topCard) {
+        $values[] = $topCard['faceup_age'];
+      }
+    }
+    sort($values);
+    foreach ($values as $value) {
+      self::drawAndScore($value);
+    }
+
+    if ($values && self::wasForeseen()) {
+      self::win();
+    }
+  }
+
+}
