18ee33fff86af50eb6d879787c7e51ca172edb3b
Micah Stairs <micah.stairs@gmail.com>
2023-09-03 16:44:59 -0400

    Fix 'The Prophecies' bug (https://boardgamearena.com/bug?id=95415)

--- modules/Innovation/Cards/Unseen/Card516.php
+++ modules/Innovation/Cards/Unseen/Card516.php
@@ -14,12 +14,7 @@ class Card516 extends Card
 
   public function initialExecution()
   {
-    // TODO(4E): This is a bug. The player can deliberately choose a choice which will fail.
-    if ($this->game->countCardsInLocation(self::getPlayerId(), 'safe') > 0) {
-      self::setMaxSteps(1);
-    } else {
-      self::drawAndSafeguard(4);
-    }
+    self::setMaxSteps(1);
   }
 
   public function getInteractionOptions(): array
@@ -27,14 +22,11 @@ class Card516 extends Card
     if (self::isFirstInteraction()) {
       return ['choices' => [0, 1]];
     } else if (self::isSecondInteraction()) {
-      return [
-        'location_from' => 'safe',
-        'location_to'   => 'none',
-      ];
+      return ['choose_from' => 'safe'];
     } else {
       return [
         'location_from' => 'safe',
-        'location_to'   => 'board',
+        'meld_keyword'  => 'true',
         'not_id'        => self::getAuxiliaryValue2(),
       ];
     }
@@ -45,18 +37,18 @@ class Card516 extends Card
     if (self::isSecondInteraction()) {
       $card = self::drawAndReveal(self::getLastSelectedAge() + 1);
       if (self::isRed($card) || self::isPurple($card)) {
-        self::setAuxiliaryValue2(self::getLastSelectedId());
+        self::setAuxiliaryValue2(self::getLastSelectedId()); // Track chosen secret
         self::setMaxSteps(3);
       } else {
         self::transferToHand($card);
       }
     } else if (self::isThirdInteraction()) {
-      $revealedCard = self::getCards('revealed')[0];
+      $revealedCard = self::getRevealedCard();
       if (self::getNumChosen() > 0) {
         self::safeguard($revealedCard);
         // Put all revealed cards in hand if they can't fit in the safe
         foreach (self::getCards('revealed') as $card) {
-            self::transferToHand($card);
+          self::transferToHand($card);
         }
       } else {
         self::transferToHand($revealedCard);
@@ -68,24 +60,15 @@ class Card516 extends Card
   protected function getPromptForListChoice(): array
   {
     return self::buildPromptFromList([
-      0 => [clienttranslate('Draw and reveal a card of value one higher than one your secrets'), []],
+      0 => [clienttranslate('Draw and reveal a card of value one higher than one your secrets')],
       1 => [clienttranslate('Draw and safeguard a ${age}'), 'age' => self::renderValue(4)],
     ]);
   }
 
   public function handleSpecialChoice(int $choice): void
   {
-    $secrets = self::getCards('safe');
     if ($choice === 1) {
       self::drawAndSafeguard(4);
-    } else if (count($secrets) === 1) {
-      $secret = $secrets[0];
-      $card = self::drawAndReveal($secret['faceup_age'] + 1);
-      if (self::isRed($card) || self::isPurple($card)) {
-        self::safeguard($card);
-      } else {
-        self::transferToHand($card);
-      }
     } else {
       self::setMaxSteps(2);
     }
