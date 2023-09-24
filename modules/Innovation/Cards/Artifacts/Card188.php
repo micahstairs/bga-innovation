60fab9b4c2b0278c84035d330ebe3a733b56d889
Micah Stairs <micah.stairs@gmail.com>
2023-09-23 21:12:10 -0400

    Implement 4th edition of Battleship Yamato (#1337)

---
+++ modules/Innovation/Cards/Artifacts/Card188.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Innovation\Cards\Artifacts;
+
+use Innovation\Cards\AbstractCard;
+
+class Card188 extends AbstractCard
+{
+  // Battleship Yamato
+
+  public function initialExecution()
+  {
+    // NOTE: There are no effects on the card.
+  }
+
+}
