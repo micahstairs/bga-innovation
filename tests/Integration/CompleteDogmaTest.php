6b5494640fdfa92fc12306cb8ff106f869814389
Micah Stairs <micah.stairs@gmail.com>
2023-10-14 12:50:40 -0400

    Add integration tests which executes all dogmas (#1383)

---
+++ tests/Integration/CompleteDogmaTest.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Integration\Cards\Base;
+
+use Integration\BaseIntegrationTest;
+
+class CompleteDogmaTest extends BaseIntegrationTest
+{
+
+  public function test_allDogmas_thirdEdition_base()
+  {
+    self::executeCards(range(1, 104));
+  }
+
+  public function test_allDogmas_fourthEdition_base()
+  {
+    self::executeCards(array_merge(range(1, 104), range(440, 449)));
+  }
+
+  public function test_allDogmas_thirdEdition_artifacts()
+  {
+    self::executeCards(range(110, 214));
+  }
+
+  public function test_allDogmas_fourthEdition_artifacts()
+  {
+    self::executeCards(array_merge(range(110, 214), range(450, 459)));
+  }
+
+  public function test_allDogmas_thirdEdition_echoes()
+  {
+    self::executeCards(range(330, 434));
+  }
+
+  public function test_allDogmas_fourthEdition_echoes()
+  {
+    self::executeCards(array_merge(range(330, 434), range(450, 459)));
+  }
+
+  public function test_allDogmas_fourthEdition_unseen()
+  {
+    self::executeCards(range(480, 594));
+  }
+
+  private function executeCards(array $cardIds)
+  {
+    shuffle($cardIds);
+
+    error_log("*** STARTING GAME ***");
+
+    $numCardsTested = 0;
+    foreach ($cardIds as $cardId) {
+
+      // Handle free action at start of turn
+      if (self::getCurrentStateName() === 'artifactPlayerTurn') {
+        self::passArtifact();
+      }
+
+      $card = $this->tableInstance->getTable()->getCardInfo($cardId);
+      $meldedCard = $this->tableInstance->getTable()->meldCard($card, self::getActivePlayerId());
+      self::dogma($meldedCard['id']);
+      $numCardsTested++;
+
+      if (self::getCurrentStateName() === 'gameEnd') {
+        error_log("*** GAME ENDED PREMATURELY ***");
+        break;
+      }
+    }
+
+    $totalCards = count($cardIds);
+    error_log("*** TESTED $numCardsTested/$totalCards CARDS ***");
+
+  }
+
+  private function passArtifact()
+  {
+    error_log("* PASS ARTIFACT");
+    $this->tableInstance
+      ->createActionInstanceForCurrentPlayer(self::getActivePlayerId())
+      ->passArtifactOnDisplay();
+    $this->tableInstance->advanceGame();
+  }
+
+  private function dogma($cardId)
+  {
+    $cardName = $this->tableInstance->getTable()->getCardName($cardId);
+    error_log("* DOGMA $cardName");
+    $this->tableInstance
+      ->createActionInstanceForCurrentPlayer(self::getActivePlayerId())
+      ->stubArg('card_id', $cardId)
+      ->dogma();
+    $this->tableInstance->advanceGame();
+
+    self::excecuteInteractions();
+  }
+
+}
