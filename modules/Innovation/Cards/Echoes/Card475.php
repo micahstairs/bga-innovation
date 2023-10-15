1b087fe1b1ee78f92105b11749d0ab48ef7172c5
Micah Stairs <micah.stairs@gmail.com>
2023-10-14 21:14:25 -0400

    Fix Robocar bug (https://boardgamearena.com/bug?id=96691)

--- modules/Innovation/Cards/Echoes/Card475.php
+++ modules/Innovation/Cards/Echoes/Card475.php
@@ -11,8 +11,6 @@ class Card475 extends AbstractCard
   //   - Choose an opponent. That player chooses a card (unrevealed) in your hand. Meld the chosen
   //     card. If you do, and it is your turn, self-execute the card, then repeat this effect.
 
-  // TODO(4E): Fix bug when this is repeated.
-
   public function hasPostExecutionLogic(): bool
   {
     return true;
@@ -28,13 +26,13 @@ class Card475 extends AbstractCard
     if (self::isFirstInteraction()) {
       return [
         'choose_player' => true,
-        'players' => $this->game->getActiveOpponents(self::getPlayerId()),
+        'players'       => $this->game->getActiveOpponents(self::getPlayerId()),
       ];
     } else {
       return [
-        'player_id' => self::getAuxiliaryValue(),
+        'player_id'     => self::getAuxiliaryValue(),
         'location_from' => 'hand',
-        'meld_keyword' => true,
+        'meld_keyword'  => true,
       ];
     }
   }
@@ -44,10 +42,10 @@ class Card475 extends AbstractCard
     self::setAuxiliaryValue($playerId); // Track opponent chosen
   }
 
-  public function handleCardChoice(array $card) {
+  public function handleCardChoice(array $card)
+  {
     if (self::isTheirTurn()) {
       self::selfExecute($card);
-      self::setNextStep(1);
     }
   }
 
