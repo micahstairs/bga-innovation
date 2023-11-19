80757702a49e0218e686ee30afbc8429517cfb61
Micah Stairs <micah.stairs@gmail.com>
2023-11-19 13:23:44 -0500

    Update Toilet (#1436)

--- material.inc.php
+++ material.inc.php
@@ -1932,8 +1932,9 @@ $this->textual_card_infos = array(
         'name'                            => clienttranslate('Toilet'),
         'echo_effect'                     => clienttranslate('Draw and tuck a ${age_4}.'),
         'i_demand_effect_first_and_third' => clienttranslate('${I demand} you return all cards from your score pile of value matching the highest bonus on my board!'),
-        'i_demand_effect_fourth'          => clienttranslate('${I demand} you return all cards from your score pile of the highest value matching a bonus on my board!'),
+        'i_demand_effect_fourth'          => clienttranslate('${I demand} you return a card from your score pile matching each different bonus value on my board!'),
         'non_demand_effect_1'             => clienttranslate('You may return a card in your hand and draw a card of the same value.'),
+        'separate_4E_implementation'      => true,
     ),
 
     /* Echoes - Age 5 */
---
+++ modules/Innovation/Cards/Echoes/Card374_4E.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Innovation\Cards\Echoes;
+
+use Innovation\Cards\AbstractCard;
+use Innovation\Enums\Locations;
+
+class Card374_3E extends AbstractCard
+{
+
+  // Toilet (3rd edition):
+  //  - ECHO: Draw and tuck a [4].
+  //  - I DEMAND you return a card from your score pile matching each different bonus value on my board!
+  //  - You may return a card in your hand and draw a card of the same value.
+
+  public function initialExecution()
+  {
+    if (self::isEcho()) {
+      self::drawAndTuck(4);
+    } else if (self::isDemand()) {
+      $values = array_unique(self::getBonuses());
+      if ($values) {
+        self::setAuxiliaryArray($values); // Store the values to be returned
+        self::setMaxSteps(1);
+      }
+    } else if (self::isFirstNonDemand()) {
+      self::setMaxSteps(1);
+    }
+  }
+
+  public function getInteractionOptions(): array
+  {
+    if (self::isDemand()) {
+      $values = self::getAuxiliaryArray();
+      return [
+        'n'                 => count($values),
+        'choose_value'      => true,
+        'age'               => $values,
+        'refresh_selection' => true,
+      ];
+    } else {
+      return [
+        'can_pass'       => true,
+        'location_from'  => Locations::HAND,
+        'return_keyword' => true,
+      ];
+    }
+  }
+
+  public function handleCardChoice(array $card)
+  {
+    if (self::isNonDemand()) {
+      self::draw(self::getValue($card));
+    } else if (self::isDemand()) {
+      self::removeFromAuxiliaryArray(self::getValue($card));
+    }
+  }
+
+}
