3699aacea9f32df52383ba93598aea37818d20c0
Micah Stairs <micah.stairs@gmail.com>
2024-08-25 22:16:51 -0400

    Add missing files

---
+++ modules/Innovation/Cards/Base/Card45.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Innovation\Cards\Base;
+
+use Innovation\Cards\AbstractCard;
+use Innovation\Enums\Colors;
+
+class Card45 extends AbstractCard
+{
+  // Chemistry:
+  //   - You may splay your blue cards right.
+  //   - Draw and score a card of value one higher than the highest top card on your board and then
+  //     return a card from your score pile.
+
+  public function initialExecution()
+  {
+    if (self::isFirstNonDemand()) {
+      self::setMaxSteps(1);
+    } else {
+      self::drawAndScore(self::getMaxValue(self::getTopCards()) + 1);
+      self::setMaxSteps(1);
+    }
+  }
+
+  public function getInteractionOptions(): array
+  {
+    if (self::isFirstNonDemand()) {
+      return self::youMay()->splayRight()->withColor(Colors::BLUE)->build();
+    } else {
+      return self::youMay()->return()->fromYourScore()->build();
+    }
+  }
+
+}
