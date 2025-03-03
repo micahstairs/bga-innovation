7e382de390172fb391ffaff12513778b60b084cb
Micah Stairs <micah.stairs@gmail.com>
2025-03-02 22:29:35 -0500

    Fix Crossbow

--- innovation.game.php
+++ innovation.game.php
@@ -11309,7 +11309,7 @@ class Innovation extends Table
             || (array_key_exists('choose_value', $options) && (array_key_exists('age', $options) && empty($options['age'])))
             || (array_key_exists('choices', $options) && empty($options['choices']))
             || (array_key_exists('color', $options) && empty($options['color']))
-            || (array_key_exists('choose_player', $options) && empty($options['player_array']))
+            || (array_key_exists('choose_player', $options) && empty($options['players']))
         ) {
 
             self::notifyIfLocationLimitShrunkSelection($executionState->getPlayerId());
