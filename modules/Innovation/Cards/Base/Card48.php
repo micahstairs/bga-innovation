d86ec4835d7a294be02a2c6e0fea03c578f3e2b4
Micah Stairs <micah.stairs@gmail.com>
2026-03-08 15:15:24 -0400

    Fix "The Pirate Code" bug (https://boardgamearena.com/bug?id=209015)

--- modules/Innovation/Cards/Base/Card48.php
+++ modules/Innovation/Cards/Base/Card48.php
@@ -25,7 +25,7 @@ class Card48 extends AbstractCard
   public function initialExecution()
   {
     if (self::isDemand() || self::getAuxiliaryValue() >= 1) {
-      self::setMaxSteps(2);
+      self::setMaxSteps(1);
     }
   }
 
